d353f6e1da30f1a80179132b50f710000053b823
Requestedd <61982807+Requestedd@users.noreply.github.com>
2020-08-13 15:15:48 +0300

    Update myhistory.py

--- myhistory.py
+++ myhistory.py
@@ -6,11 +6,6 @@ import time
 import threading 
 from time import sleep
 import os
-from os import environ
-from flask import Flask
-
-app = Flask(__name__)
-app.run(environ.get('PORT'))
 
 token = os.environ.get('bot_token')
 ids = os.environ.get('bot_ids')
